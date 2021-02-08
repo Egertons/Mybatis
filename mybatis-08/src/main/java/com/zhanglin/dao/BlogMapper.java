4b3f9d02e9c6395971bff55baab088a15c31296a
egert <egertons@163.com>
2021-02-08 08:38:03 +0800

    2021_2_8_08_37

--- mybatis-08/src/main/java/com/zhanglin/dao/BlogMapper.java
+++ mybatis-08/src/main/java/com/zhanglin/dao/BlogMapper.java
@@ -2,9 +2,16 @@ package com.zhanglin.dao;
 
 import com.zhanglin.pojo.Blog;
 
+import java.util.List;
+import java.util.Map;
+
 public interface BlogMapper {
     //新建测试
    public Blog getAll();
 
+   public int add(Blog blog);
 
+   public List<Blog> quaryBlogByIf(Map map);
+
+   public List<Blog> quaryBlogPowerByChoose(Map map);
 }
--- mybatis-08/src/test/java/com/zhanglin/dao/MyTest.java
+++ mybatis-08/src/test/java/com/zhanglin/dao/MyTest.java
@@ -1,9 +1,15 @@
 package com.zhanglin.dao;
 
 import com.zhanglin.pojo.Blog;
+import com.zhanglin.utils.IDutils;
 import com.zhanglin.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MyTest {
     @Test
@@ -12,6 +18,75 @@ public class MyTest {
         BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
         Blog all = mapper.getAll();
         System.out.println(all.toString());
+        sqlSession.close();
+    }
+
+    @Test
+    public void a(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        Blog blog = new Blog();
+        blog.setId(IDutils.getId());
+        blog.setTitle("text");
+        blog.setAuthor("zhanglin");
+        blog.setCreateTime(new Date());
+        blog.setViews(888);
+        int add = mapper.add(blog);
+
+        blog.setId(IDutils.getId());
+        blog.setTitle("java");
+        blog.setCreateTime(new Date());
+        blog.setViews(9999);
+        int add1 = mapper.add(blog);
+
+        blog.setId(IDutils.getId());
+        blog.setTitle("python");
+        blog.setCreateTime(new Date());
+        blog.setViews(6666);
+        int add2 = mapper.add(blog);
+
+        blog.setId(IDutils.getId());
+        blog.setTitle("C");
+        blog.setCreateTime(new Date());
+        blog.setViews(7777);
+        int add3 = mapper.add(blog);
+
+        blog.setId(IDutils.getId());
+        blog.setTitle("JS");
+        blog.setCreateTime(new Date());
+        blog.setViews(2222);
+        int add4 = mapper.add(blog);
+
+        if (add>0 && add1>0 && add2>0 && add3>0 && add4>0){
+            System.out.println("全部添加成功");
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void quaryBlogByIf(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        Map map = new HashMap();
+        map.put("title","text");
+        map.put("author","zhanglin");
+        List<Blog> blogs = mapper.quaryBlogByIf(map);
+        for (Blog blog : blogs) {
+            System.out.println(blog.toString());
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void quaryBlogPowerByChoose(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        Map map = new HashMap();
+        map.put("demo",1);
+        List<Blog> blogs = mapper.quaryBlogPowerByChoose(map);
+        for (Blog blog : blogs) {
+            System.out.println(blog.toString());
+        }
         sqlSession.close();
     }
 }
